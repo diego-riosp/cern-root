a0ea4556496dc85eb8f1e69aa59bfdf8420b61d2
diego-riosp <diego.riosp@udea.edu.co>
2025-02-19 00:08:05 +0000

    Including new OS:Arch

---
+++ build-arch/CMakeFiles/program.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/program.dir/link.d"
+  "CMakeFiles/program.dir/CMakeFiles/3.31.5/CompilerIdCXX/CMakeCXXCompilerId.cpp.o"
+  "CMakeFiles/program.dir/CMakeFiles/3.31.5/CompilerIdCXX/CMakeCXXCompilerId.cpp.o.d"
+  "program"
+  "program.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/program.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
